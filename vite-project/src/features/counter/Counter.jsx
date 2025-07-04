70e0d189faa0cb00891fa19c7a14dd15e1de3ea3
rakshith_s <rakshithsantosh@gmail.com>
2025-07-04 07:53:20 +0530

    implemented reset and incrementbyamount functions

--- vite-project/src/features/counter/Counter.jsx
+++ vite-project/src/features/counter/Counter.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./counterSlice";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
+  const [amount, setAmount] = React.useState(0);
+  const increvalue = Number(amount) || 0;
+  const resetValue = () => {
+    setAmount(0);
+    dispatch(reset());
+  };
   return (
     <div>
       <p>{count}</p>
+
       <button onClick={() => dispatch(increment())}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
+      <div>
+        <input
+          type="text"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            dispatch(incrementByAmount(increvalue));
+          }}
+        >
+          incrementByAmount
+        </button>
+      </div>
+      <button onClick={() => resetValue()}>reset</button>
     </div>
   );
 };
